5509f47328fbb2196041fe91206dd0f3f17cf87a
la1kov-alex <laikov000307@gmail.com>
2025-08-31 23:41:09 +0300

    Update custom-massage.js

--- src/modules/custom-massage.js
+++ src/modules/custom-massage.js
@@ -1,36 +1,46 @@
-export function showCustomMessage(text, duration = 3000) {
-    const message = document.createElement('div')
-    message.innerText = text
+import { Module } from "../core/module";
 
-    Object.assign(message.style, {
-        position: 'fixed',
-        right: '20px',
-        bottom: '50px',
-        background: 'lightcyan',
-        padding: '13px 20px',
-        borderRadius: '20px',
-        boxShadow: '0 2px 6px rgba(42, 34, 34, 0.2)',
-        cursor: 'default'
-    })
+export class ShowCustomMessage extends Module {
+    constructor(text, duration = 3000) {
+        super('message', 'Кастомное сообщение');
+        this.text = text;
+        this.duration = duration;
+    }
 
-    const closeBtn = document.createElement('span')
-    closeBtn.innerText = '×'
-    Object.assign(closeBtn.style, {
-        marginLeft: '18px',
-        cursor: 'pointer',
-        fontWeight: 'bold'
-    })
-    closeBtn.onclick = () => document.body.removeChild(message)
+    trigger() {
+        const message = document.createElement('div');
+        message.innerText = this.text;
 
-    message.appendChild(closeBtn)
-    document.body.appendChild(message)
+        Object.assign(message.style, {
+            position: 'fixed',
+            right: '20px',
+            bottom: '50px',
+            background: 'lightcyan',
+            padding: '13px 20px',
+            borderRadius: '20px',
+            boxShadow: '0 2px 6px rgba(42, 34, 34, 0.2)',
+            cursor: 'default'
+        });
 
-    let timer = setTimeout(() => {
-        document.body.removeChild(message)
-    }, duration)
+        const closeBtn = document.createElement('span');
+        closeBtn.innerText = '×';
+        Object.assign(closeBtn.style, {
+            marginLeft: '18px',
+            cursor: 'pointer',
+            fontWeight: 'bold'
+        });
+        closeBtn.onclick = () => document.body.removeChild(message);
 
-    message.onmouseenter = () => clearTimeout(timer)
-    message.onmouseleave = () => {
-        timer = setTimeout(() => document.body.removeChild(message), duration)
+        message.appendChild(closeBtn);
+        document.body.appendChild(message);
+
+        let timer = setTimeout(() => {
+            document.body.removeChild(message);
+        }, this.duration);
+
+        message.onmouseenter = () => clearTimeout(timer);
+        message.onmouseleave = () => {
+            timer = setTimeout(() => document.body.removeChild(message), this.duration);
+        };
     }
 }
