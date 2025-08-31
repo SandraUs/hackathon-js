5cbd043182c0aed36a1340832680dd034768940d
la1kov-alex <laikov000307@gmail.com>
2025-08-31 23:40:43 +0300

    Update background.module.js

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -4,7 +4,7 @@ import {getRandomColor} from "@/utils";
 
 export class BackgroundModule extends Module {// type: background, text: Поменять цвет
   constructor() {
-    super('background', 'random-background');
+    super('background', 'Поменять цвет фона');
   }
   trigger() {
     document.body.style.backgroundImage = getRandomColor();
