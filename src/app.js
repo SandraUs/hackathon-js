9642bb5f0bc848c96e541671b443a2e51331e7de
Pavel <you@example.com>
2025-08-31 20:20:06 +0300

    фон цвет

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
 import './styles.css'
-
+import {BackgroundModule} from "@/modules/background.module";
+import {BackgroundDefaultModule} from "@/modules/background-default.module";
+import {HowMuchTimeSpentModule} from "@/modules/howmuch-time-spent.module";
