90dc2d4b91092e1a6ce42984b6f3a94eb0ad5df8
Achilovraf <Achilovraf@gmail.com>
2025-08-31 23:35:32 +0500

    Добавил модуль random.sound

--- src/app.js
+++ src/app.js
@@ -1,27 +1,30 @@
-import './modules/css/timer.css';
-import { TimerModule } from './modules/timer.module';
+import "./modules/css/timer.css";
+import "./modules/css/random.sound.css";
+import { TimerModule } from "./modules/timer.module";
 
-import './styles.css'
+import "./styles.css";
 
-import {BackgroundModule} from "@/modules/background.module";
-import {BackgroundDefaultModule} from "@/modules/background-default.module";
-import {HowMuchTimeSpentModule} from "@/modules/howmuch-time-spent.module";
+import { BackgroundModule } from "@/modules/background.module";
+import { BackgroundDefaultModule } from "@/modules/background-default.module";
+import { HowMuchTimeSpentModule } from "@/modules/howmuch-time-spent.module";
+import { RandomSoundModule } from "./modules/random.sound.module";
 
-import { ContextMenu } from './menu.js'
-import { ShapeModule } from './modules/shape.module.js'
+import { ContextMenu } from "./menu.js";
+import { ShapeModule } from "./modules/shape.module.js";
 
+// Сначала создаем menu
+const menu = new ContextMenu("#menu");
 
-menu.add(new ShapeModule())
-
-const menu = new ContextMenu('#menu');
+// Затем добавляем модули
+menu.add(new ShapeModule());
 menu.add(new TimerModule());
+menu.add(new RandomSoundModule());
 
-document.addEventListener('contextmenu', event => {
-    event.preventDefault();
-    menu.open(event.clientX, event.clientY)
-})
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  menu.open(event.clientX, event.clientY);
+});
 
-document.addEventListener('click', () => {
-    menu.close();
-})
-
+document.addEventListener("click", () => {
+  menu.close();
+});
--- src/menu.js
+++ src/menu.js
@@ -1,86 +1,43 @@
-import {Menu} from './core/menu';
-
-
-export default class ContextMenu extends Menu {
-    constructor(selector) {
-        super(selector);
-        this.modules = [];
-    }
-
-    open(x, y) {
-        if(this.modules.length === 0) {
-            return
-        }
-
-        this.el.innerHTML = '';
-
-        this.modules.forEach(module => {
-            this.el.insertAdjacentHTML('beforeend', module.toHTML());
-        })
-
-        this.el.style.top = y + 'px';
-        this.el.style.left = x + 'px';
-        this.el.classList.add('open');
-
-        this.el.querySelectorAll('.menu-item').forEach(item => {
-            item.addEventListener('click', () => {
-                const type = item.dataset.type;
-                const mod = this.modules.find(m => m.type === type);
-                if(mod) {
-                    mod.trigger();
-                }
-                this.close();
-            })
-        })
-    }
-    
-    close() {
-        this.el.classList.remove('open');
-    }
-
-    add(module) {
-        this.modules.push(module);
-    }
-}
+import { Menu } from "./core/menu";
 
 export class ContextMenu extends Menu {
   constructor(selector) {
-    super(selector)
-
-    document.addEventListener('contextmenu', event => {
-      event.preventDefault()
-      this.open(event.pageX, event.pageY)
-    })
+    super(selector);
+    this.modules = [];
   }
 
   open(x, y) {
-    if (this.items.length === 0) return
+    if (this.modules.length === 0) {
+      return;
+    }
 
-    this.el.innerHTML = ''
-    this.items.forEach(module => {
-      this.el.innerHTML += module.toHTML()
-    })
+    this.el.innerHTML = "";
 
-    this.el.style.left = `${x}px`
-    this.el.style.top = `${y}px`
-    this.el.style.display = 'block'
+    this.modules.forEach((module) => {
+      this.el.insertAdjacentHTML("beforeend", module.toHTML());
+    });
 
-    // добавляем обработчики для каждого li
-    this.el.querySelectorAll('.menu-item').forEach(li => {
-      li.addEventListener('click', () => {
-        const module = this.items.find(m => m.type === li.dataset.type)
-        if (module) module.trigger()
-        this.close()
-      })
-    })
+    this.el.style.top = y + "px";
+    this.el.style.left = x + "px";
+    this.el.classList.add("open");
+
+    this.el.querySelectorAll(".menu-item").forEach((item) => {
+      item.addEventListener("click", () => {
+        const type = item.dataset.type;
+        const mod = this.modules.find((m) => m.type === type);
+        if (mod) {
+          mod.trigger();
+        }
+        this.close();
+      });
+    });
   }
 
   close() {
-    this.el.style.display = 'none'
+    this.el.classList.remove("open");
   }
 
   add(module) {
-    this.items.push(module)
+    this.modules.push(module);
   }
-  }
-
+}
