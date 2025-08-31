e63a8cca72977fc82f199ce0d346f868157c1a94
SandraUs <66241283+SandraUs@users.noreply.github.com>
2025-09-01 02:34:44 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,39 +6,32 @@ import { Module } from "./core/module";
 import { RandomSoundModule } from "./modules/random-sound";
 import { ClicksModule } from './modules/clicks.module';
 import { TimerModule } from './modules/timer.module';
-import {BackgroundModule} from "@/modules/background.module";
-import {BackgroundDefaultModule} from "@/modules/background-default.module";
-import {HowMuchTimeSpentModule} from "@/modules/howmuch-time-spent.module";
-import { ContextMenu } from './menu.js'
-import { ShapeModule } from './modules/shape.module.js'
+import { BackgroundModule } from './modules/background.module';
+import { BackgroundDefaultModule } from './modules/background-default.module';
+import { HowMuchTimeSpentModule } from './modules/howmuch-time-spent.module';
+import { ContextMenu } from './menu.js';
+import { ShapeModule } from './modules/shape.module.js';
 
 showCustomMessage("Привет! Мы успешно завершили модуль по JavaScript! 🎉", 4000);
 
 const menu = new ContextMenu('#menu');
+
 menu.add(new TimerModule());
 menu.add(new ShapeModule());
-menu.add(new showCustomMessage());
 menu.add(new ClicksModule());
 menu.add(new RandomSoundModule());
 menu.add(new BackgroundModule());
 menu.add(new BackgroundDefaultModule());
 menu.add(new HowMuchTimeSpentModule());
-  
+//menu.add(new TestModule1());
+//enu.add(new TestModule2());
+menu.add(new RandomSoundModule());
+
 document.addEventListener('contextmenu', event => {
     event.preventDefault();
     menu.open(event.clientX, event.clientY)
-})
+});
 
 document.addEventListener('click', () => {
     menu.close();
-})
-
-
-// Добавляем модули
-contextMenu.add(new TestModule1());
-contextMenu.add(new TestModule2());
-contextMenu.add(new RandomSoundModule());
-
-document.addEventListener('click', () => {
-    menu.close();
-})
+});
