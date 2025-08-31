adc88793ce5b5198e005badaa407d4aad31c9142
SandraUs <66241283+SandraUs@users.noreply.github.com>
2025-09-01 02:14:37 +0700

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,56 +1,42 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu'
 
+export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector)
 
-export default class ContextMenu extends Menu {
-    constructor(selector) {
-        super(selector);
-        this.modules = [];
-    }
-
-
-    // Показываем меню по правому клику
-    document.addEventListener("contextmenu", (event) => {
-      event.preventDefault();
-      this.open(event.clientX, event.clientY);
-    });
-
-
-    // Закрываем меню по Escape
-    document.addEventListener("keydown", (event) => {
-      if (event.key === "Escape") {
-        this.close();
-      }
-    });
+    document.addEventListener('contextmenu', event => {
+      event.preventDefault()
+      this.open(event.pageX, event.pageY)
+    })
   }
-}
 
-        this.el.innerHTML = '';
+  open(x, y) {
+    if (this.items.length === 0) return
 
-        this.modules.forEach(module => {
-            this.el.insertAdjacentHTML('beforeend', module.toHTML());
-        })
+    this.el.innerHTML = ''
+    this.items.forEach(module => {
+      this.el.innerHTML += module.toHTML()
+    })
 
-        this.el.style.top = y + 'px';
-        this.el.style.left = x + 'px';
-        this.el.classList.add('open');
+    this.el.style.left = `${x}px`
+    this.el.style.top = `${y}px`
+    this.el.style.display = 'block'
 
-        this.el.querySelectorAll('.menu-item').forEach(item => {
-            item.addEventListener('click', () => {
-                const type = item.dataset.type;
-                const mod = this.modules.find(m => m.type === type);
-                if(mod) {
-                    mod.trigger();
-                }
-                this.close();
-            })
-        })
-    }
-    
-    close() {
-        this.el.classList.remove('open');
-    }
+    // добавляем обработчики для каждого li
+    this.el.querySelectorAll('.menu-item').forEach(li => {
+      li.addEventListener('click', () => {
+        const module = this.items.find(m => m.type === li.dataset.type)
+        if (module) module.trigger()
+        this.close()
+      })
+    })
+  }
 
-    add(module) {
-        this.modules.push(module);
-    }
+  close() {
+    this.el.style.display = 'none'
+  }
+
+  add(module) {
+    this.items.push(module)
+  }
 }
